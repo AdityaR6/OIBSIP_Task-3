14fd1cb77e7c92464db4f725eb307879b63e3dde
AdityaR6 <78144479+AdityaR6@users.noreply.github.com>
2022-11-21 22:09:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let input = document.getElementById('input');
+let tasks = document.getElementById('tasks');
+
+function addTask(){
+    if(input.value.length == 0){
+        alert("Please Enter a Task")
+    }
+    else{
+        tasks.innerHTML += `<div id="temp">
+        <span id="taskname">
+        ${input.value}
+        </span>
+         
+        <button class="done" onclick ="done(this)" ondblclick="rmv(this)"><i class="fa-solid fa-circle-check"></i></button>
+        <button class="del" onclick="del(this)"><i class="fa-solid fa-times-circle"></i></button>
+        
+        </div>`;
+    }
+    input.value = ""
+}
+
+function del(e) {
+    e.parentNode.remove();      
+}
+
+function done(e) {
+    e.classList.toggle('completed');
+    e.parentNode.style.textDecoration = "line-through"; 
+    
+}
+
+function rmv(e) {
+    e.parentNode.style.textDecoration = "none";
+    e.classList.toggle('complete');
+}
+
+
+
+
